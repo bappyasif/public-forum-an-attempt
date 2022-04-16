55014f95514f1b2505492256516a94755113fcc9
bappyasif <bappyasif@hotmail.com>
2022-04-16 18:59:46 +0600

    initial project setup with rca and components container

---
+++ web-forum/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponentsContainer from './componenets/components-container';
+
+function App() {
+  return (
+    <div className="App">
+      <ComponentsContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ web-forum/src/componenets/components-container.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ComponentsContainer() {
+  return (
+    <div>ComponenetsContainer</div>
+  )
+}

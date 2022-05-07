2124f1e116db375fdfc9612b0b4c00436e082f9c
bappyasif <bappyasif@hotmail.com>
2022-05-07 21:11:39 +0600

    new topic ui is in view, along with variable usages for css

--- src/components/forum-dashboard/header-content/index.js
+++ src/components/forum-dashboard/header-content/index.js
@@ -4,13 +4,16 @@ import { RenderListOfElements } from '../../general-purpose-use-hof';
 import './styles.css'
 
 export function ForumDashboardHeaderUI() {
-    let allStates = useContext(UserContext);
-    let listLabels = () => allStates.headerLables && <RenderListOfElements list={allStates.headerLables} wrapperClassName='rest-labels' elementClassName={'header-label'} />
+  let allStates = useContext(UserContext);
+  let listLabels = () => allStates.headerLables && <RenderListOfElements list={allStates.headerLables} wrapperClassName='rest-labels' elementClassName={'header-label'} />
 
   return (
     <nav aria-label='dashboard-header-category-navigation' className='dahsboard-header-ui-container'>
+      <div className='categories-labels'>
         <div className='all-categories-label'>All Categories</div>
         {listLabels()}
+      </div>
+      <button aria-label='new topic' id='new-topic'>New Topic</button>
     </nav>
   )
 }

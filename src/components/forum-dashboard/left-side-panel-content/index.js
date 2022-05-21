1d35a94e3029055b39f185119f61cc2eeee37385
bappyasif <bappyasif@hotmail.com>
2022-05-21 20:37:03 +0600

    used grid instead of table for left hand side dashkboard panel

--- src/components/forum-dashboard/left-side-panel-content/index.js
+++ src/components/forum-dashboard/left-side-panel-content/index.js
@@ -3,18 +3,65 @@ import { UserContext } from '../../components-container'
 import './styles.css'
 
 
+// export function DashboardPanelLeftSide() {
+//   let allStates = useContext(UserContext)
+//   return (
+//     <aside className='left-side-panel-container'>
+//       <table>
+//         <caption>List of categories and number of topics currently in it</caption>
+//         <TableHeader />
+//         <tbody>
+//           <RenderListOfTableRowElements list={allStates.categoriesInfo} />
+//         </tbody>
+//       </table>
+//     </aside>
+//   )
+// }
+
+
 export function DashboardPanelLeftSide() {
   let allStates = useContext(UserContext)
   return (
     <aside className='left-side-panel-container'>
-      <table>
+      <LeftSideGridHeader />
+      <LeftSideGridTopics list={allStates.categoriesInfo} />
+      {/* <table>
         <caption>List of categories and number of topics currently in it</caption>
         <TableHeader />
         <tbody>
           <RenderListOfTableRowElements list={allStates.categoriesInfo} />
         </tbody>
-      </table>
+      </table> */}
     </aside>
+  )
+}
+
+let LeftSideGridTopics = ({list}) => {
+  let renderTopics = () => list?.map(item => <RenderTopic key={item.name} topicItem={item} />)
+  return (
+    <ul className='lsp-topic-list'>
+      {renderTopics()}
+    </ul>
+  )
+}
+
+let RenderTopic = ({topicItem}) => {
+  let {name, topics} = {...topicItem}
+  return (
+    <li>
+      <p>{name}</p>
+      <p>{topics}</p>
+    </li>
+  )
+}
+
+let LeftSideGridHeader = () => {
+  let headers = ['Categories', 'Topics'];
+  let renderHeaders = () => headers.map(name => <li key={name}>{name}</li>)
+  return (
+    <ul className='lsp-headers'>
+      {renderHeaders()}
+    </ul>
   )
 }
 

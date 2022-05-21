9ce83a28d78c5d48539ea4b2d0689392a51a7dc8
bappyasif <bappyasif@hotmail.com>
2022-05-22 01:17:14 +0600

    trying out routes functionalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 // import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ComponentsContainer from './components/components-container';
 
 function App() {
+  let baseUri = '/public-forum-an-attempt'
   return (
     <div className="App">
-      <ComponentsContainer />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path={baseUri} element={<ComponentsContainer />}>
+            <Route path={`${baseUri}/test`} element={<h4>hear hear</h4>} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <ComponentsContainer /> */}
     </div>
   );
 }

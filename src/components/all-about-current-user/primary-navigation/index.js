f84bc3cf141969dd3a4ed7156b928b3d1eb1a233
bappyasif <bappyasif@hotmail.com>
2022-08-20 23:01:21 +0600

    primary navigation enhanced interactivity
    current user primary navigation can now move through using tabs, has corresponding css for them as well

--- src/components/all-about-current-user/primary-navigation/index.js
+++ src/components/all-about-current-user/primary-navigation/index.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { baseUri } from '../../../App'
 import './styles.css'
 
 function PrimaryNavigation() {
-    let items = ['Summary', 'Activity', 'Notifications', 'Preferences']
-    let renderItems = () => items.map(item => <li key={item}>{item}</li>)
+  let [activeTab, setActiveTab] = useState(null)
+  let handleActiveTab = name => setActiveTab(name)
+
+  let items = ['Summary', 'Activity', 'Notifications', 'Preferences']
+  let renderItems = () => items.map(item => <RenderNavigationItem key={item} name={item} handleActiveTab={handleActiveTab} activeTabName={activeTab} />)
   return (
     <section className='primary-navigation'>
-        <nav>
-            <ul>
-                {renderItems()}
-            </ul>
-        </nav>
+      <nav>
+        <ul role={"tablist"}>
+          {renderItems()}
+        </ul>
+      </nav>
     </section>
+  )
+}
+
+let RenderNavigationItem = ({ name, handleActiveTab, activeTabName }) => {
+  let handleClicked = () => {
+    handleActiveTab(name);
+  }
+
+  return (
+    <li
+      role={'tab'}
+      onClick={handleClicked}
+      className={`${activeTabName === name ? 'active' : 'inactive'}`}
+      aria-selected={`${activeTabName === name ? true : false}`}
+    >
+      <Link to={`${baseUri}/u/${name}`} >{name}</Link>
+    </li>
+    // <li role={'tab'} onClick={handleClicked} className={`${clicked ? 'active' : ''}`}>{name}</li>
   )
 }
 

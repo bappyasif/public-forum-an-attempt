0cd79d28aa2299c507e7b27e5fa0fe1093f7a3d7
bappyasif <bappyasif@hotmail.com>
2022-08-27 23:38:00 +0600

    expanded profile view & aside navtab styles fixes

--- src/components/all-about-current-user/user-preferences/left-nav-panel/index.js
+++ src/components/all-about-current-user/user-preferences/left-nav-panel/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { baseUri } from '../../../../App'
 import './styles.css'
 
 function LeftNavigationPanel() {
-    let renderPanels = () => panelNames.map(name => <RenderPanelName key={name} name={name} />)
+    let [currentTab, setCurrentTab] = useState(null)
+
+    let handleCurrentTab = name => setCurrentTab(name)
+
+    let renderPanels = () => panelNames.map(name => <RenderPanelName key={name} name={name} currentTab={currentTab} handleTab={handleCurrentTab} />)
+    
     return (
         <aside>
             <ul className='left-panel-nav-list'>
@@ -12,9 +19,12 @@ function LeftNavigationPanel() {
     )
 }
 
-let RenderPanelName = ({ name }) => {
+let RenderPanelName = ({ name, currentTab, handleTab }) => {
+    let handleClick = () => {
+        handleTab(name)
+    }
     return (
-        <li>{name}</li>
+        <li onClick={handleClick} className={`nav-tab ${currentTab === name ? "active" : ""}`} >{name}</li>
     )
 }
 
--- src/components/pages/current-user/preferences.js
+++ src/components/pages/current-user/preferences.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CollapsedInfo from '../../all-about-current-user/collapsed-info'
+import ExtendedView from '../../all-about-current-user/collapsed-info/extended-view'
 import PrimaryNavigation from '../../all-about-current-user/primary-navigation'
 import UserPreferences from '../../all-about-current-user/user-preferences'
 import HeaderUI from '../../header-section'
 
 function PreferencesDetails({setAllStates}) {
+    let [expanded, setExpanded] = useState(false)
+    let handleToggle = () => setExpanded(!expanded)
+
     return (
         <main>
             <HeaderUI setAllStates={setAllStates} />
             <div style={{ width: '69vw', margin: '72px auto' }}>
+                <CollapsedInfo expanded={expanded} handleToggle={handleToggle} />
+                {expanded ? <ExtendedView /> : null}
                 <PrimaryNavigation />
                 <UserPreferences />
             </div>
